5051474624250769757321b525cc11a5c9379219
ccareyncsu <ccarey@ncsu.edu>
2018-11-08 04:13:07 -0700

    color change to overlay

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
     <div className='background'>
 
       <picture>
-        <source media="(min-width: 400px)" srcset="/images/mount-huron.jpg" alt="A picture of Huron Peak in the Rocky Mountains" />
+        <source media="(min-width: 401px)" srcset="/images/mount-huron.jpg" alt="A picture of Huron Peak in the Rocky Mountains" />
         <img src="/images/portrait-suit.jpg" alt="A picture of Chasen" />
       </picture>
       {/* <div>
