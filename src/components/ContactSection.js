5cf854d3d19e60025966619f97dd1e944de2bb99
ccareyncsu <ccarey@ncsu.edu>
2018-11-10 19:54:47 -0700

    transition to contact icons

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import {FaRegFileAlt} from 'react-icons/fa';
+import {FaRegFileAlt, FaEnvelope, FaGithub, FaLinkedinIn} from 'react-icons/fa';
 import {SocialIcon} from 'react-social-icons';
 
 export default function ContactSection (props) {
@@ -8,14 +8,37 @@ export default function ContactSection (props) {
     <div className='section' id='contact'>
       <div>
         <h2 className='teal-bar white-text left'>CONTACT</h2>
-        <h3 className='contact-message'>I look foward to hearing from you!</h3>
         <div className='social-icons'>  
-          <SocialIcon url="http://linkedin.com/in/chasen-arey" className='social-icons' title='LinkedIn'/>
-          <SocialIcon url="mailto:ccarey@ncsu.edu" className='social-icons' title='Mail'/>
-          <SocialIcon url="https://github.com/ccareyncsu" className='social-icons' title='GitHub'/>
+          {/* <SocialIcon url="http://linkedin.com/in/chasen-arey" className='social-icons' title='View Chasen on LinkedIn' color='#593C8F'/>
+          <SocialIcon url="mailto:ccarey@ncsu.edu" className='social-icons' title='Email Chasen' color='#593C8F'/>
+          <SocialIcon url="https://github.com/ccareyncsu" className='social-icons' title='View Chasen on GitHub' color='#593C8F'/> */}
+
+          <a href='http://linkedin.com/in/chasen-arey'>
+            <span title="View Chasen on LinkedIn">
+              <div className='file-background social-icon'>
+                <FaLinkedinIn className='file' title='LinkedIn'/>
+              </div>
+            </span>
+          </a>
+            <a href='https://docs.google.com/document/d/1bwLMQrKyk6diShgQhyYHkc8hdDnkKZ2stIIXwGSNKh8/edit#heading=h.gjdgxs'>
+            <span title="Email Chasen">
+              <div className='file-background social-icon'>
+                <FaEnvelope className='file' title='Email'/>
+              </div>
+            </span>
+          </a>
           <a href='https://docs.google.com/document/d/1bwLMQrKyk6diShgQhyYHkc8hdDnkKZ2stIIXwGSNKh8/edit#heading=h.gjdgxs'>
-            <span title="Resume">
-              <FaRegFileAlt className='file social-icon' title='Resume'/>
+            <span title="View Chasen on GitHub">
+              <div className='file-background social-icon'>
+                <FaGithub className='file' title='Github'/>
+              </div>
+            </span>
+          </a>
+          <a href='https://docs.google.com/document/d/1bwLMQrKyk6diShgQhyYHkc8hdDnkKZ2stIIXwGSNKh8/edit#heading=h.gjdgxs'>
+            <span title="View Chasen's Resume">
+              <div className='file-background social-icon'>
+                <FaRegFileAlt className='file' title='Resume'/>
+              </div>
             </span>
           </a>
         </div>
